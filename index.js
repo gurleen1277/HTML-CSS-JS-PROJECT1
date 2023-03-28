31637f7c925d90bde4853e411246fcab8500e76b
Gurleen Kaur <gk184151@gmail.com>
2023-03-28 20:41:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+setInterval(() => {
+    data= new Date();
+    htime = data.getHours();
+    mtime = data.getMinutes();
+    stime = data.getSeconds();
+    hrotation=30*htime+mtime/2;
+    mrotation=6*mtime;
+    srotation=6*stime;
+
+    hour.style.transform=`rotate(${hrotation}deg)`
+    minute.style.transform=`rotate(${mrotation}deg)`
+    second.style.transform=`rotate(${srotation}deg)`
+},1000);
